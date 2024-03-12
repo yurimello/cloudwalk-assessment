ea40f694b11c660c8206a4275e350592294cc0bf
Yuri Mello <yuri.mcr@gmail.com>
2024-03-12 20:41:25 -0300

    add player model #7

--- app/models/world_environment.rb
+++ app/models/world_environment.rb
@@ -1,5 +1,6 @@
 class WorldEnvironment < ApplicationRecord
   WORLD_NAME = 'world'.freeze
+  validates :name, presence: true, uniqueness: true
   
   def self.instance
     first_or_create(name: WORLD_NAME)
