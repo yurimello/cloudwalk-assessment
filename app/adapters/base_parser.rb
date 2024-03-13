8e337fbd47804c06843408ede115114c56cff51a
Yuri Mello <yuri.mcr@gmail.com>
2024-03-13 12:01:51 -0300

    parsing kills to game activity model #16

--- app/adapters/base_parser.rb
+++ app/adapters/base_parser.rb
@@ -4,6 +4,22 @@ class BaseParser
   end
 
   def games;end
+
+  def game_id(game_log_id, content);end
+
+  def kills(game_data)
+    []
+  end
+
+  def parse_kill(game_id, kill_data)
+    {
+      killer: '',
+      killer_class: '',
+      killed: '',
+      death_means: '',
+      kill_id: ''
+    }
+  end
   
   private
   def log_content;end
--- app/services/import_game_log_service.rb
+++ app/services/import_game_log_service.rb
@@ -17,6 +17,20 @@ class ImportGameLogService
     games.each do |game_data|
       game_id = parser.game_id(game_log.id, game_data)
       game = Game.find_or_create_by(game_log_id: game_log.id, checksum: game_id)
+      kills = parser.kills(game_data)
+      kills.each do |kill_data|
+        parsed_kill = parser.parse_kill(game.id, kill_data)
+        killer = parsed_kill[:killer_class].find_or_create_by(name: parsed_kill[:killer])
+        killed = Player.find_or_create_by(name: parsed_kill[:killed])
+        game_activity = GameActivity.find_by(game: game, checksum: parsed_kill[:kill_id])
+        death_means = GameActivity::DEATH_MEANS.index(parsed_kill[:death_means])
+        puts parsed_kill[:death_means]
+        if game_activity
+          game_activity.update(killerable: killer, killed: killed, death_means: death_means)
+        else
+          GameActivity.create(game: game, killerable: killer, killed: killed, death_means: death_means, checksum: parsed_kill[:kill_id])
+        end
+      end
     end
   end
 
